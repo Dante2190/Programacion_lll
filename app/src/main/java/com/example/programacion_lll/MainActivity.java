018de2cb4d1ef5ebab371a028bcf02fb1239aa30
Anny Dias <usts594018@ugb.edu.sv>
2020-09-05 22:41:02 -0600

    agregado del tabhost con sus respectivas pestañas

--- app/src/main/java/com/example/programacion_lll/MainActivity.java
+++ app/src/main/java/com/example/programacion_lll/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tbconversor = (TabHost) findViewById(R.id.tbConversor);
+        tbconversor.setup();
+        //agregar pesta�as al tabhost
+        tbconversor.addTab(tbconversor.newTabSpec("tab1").setIndicator("Conversor Propio", null).setContent(R.id.tabPropio));
+        tbconversor.addTab(tbconversor.newTabSpec("tab2").setIndicator("Conversor De Area", null).setContent(R.id.tabArea));
+
 
     }
 }
