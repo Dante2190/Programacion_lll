4af76211382a9a55bc31f8bb6e02c5a3b89e719b
DanteMan21 <denzel2190@gmail.com>
2020-11-10 19:46:25 -0600

    aaa

--- app/src/main/java/com/example/programacion_lll/AgregarPedidos.java
+++ app/src/main/java/com/example/programacion_lll/AgregarPedidos.java
@@ -2,18 +2,36 @@ package com.example.programacion_lll;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.programacion_lll.cnxsqlite.SQLiteDB;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AgregarPedidos extends AppCompatActivity {
     private EditText nPedido, edtNombre, edtDireccion, edtPedido;
@@ -26,6 +44,19 @@ public class AgregarPedidos extends AppCompatActivity {
     private String direccion = "";
     private String pedido = "";
 
+    ImageView imgProducto;
+    String urlCompletaImg;
+    Intent takePictureIntent;
+    private ImageView mUploadPicture;
+    private ImageView mImageView;
+    private Bitmap mCaptureOrUploadBitmap=null;
+    private String mProductImagePath=null;
+    String productImagePath;
+
+
+
+    private static final int UPLOAD_PICTURE=2;
+
 
 
 
@@ -33,6 +64,9 @@ public class AgregarPedidos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_pedidos);
+
+        mImageView = (ImageView) findViewById(R.id.tomarFotos);
+        imgProducto = findViewById(R.id.tomarFotos);
 
         nPedido = (EditText)findViewById(R.id.edtNumeroP) ;
         edtNombre = (EditText)findViewById(R.id.edtNombre) ;
@@ -62,6 +96,127 @@ public class AgregarPedidos extends AppCompatActivity {
             }
         });*/
         MostrarDatos();
+        TomarFotoProducto();
+
+        mUploadPicture = (ImageView) findViewById(R.id.suburImage);
+        mUploadPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                if (Build.VERSION.SDK_INT > 19) {
+                    intent = new Intent(Intent.ACTION_OPEN_DOCUMENT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                } else {
+                    intent = new Intent(Intent.ACTION_GET_CONTENT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                }
+
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), UPLOAD_PICTURE);
+            }
+        });
+
+    }
+
+    //fotos desde aqui
+    void TomarFotoProducto(){
+        imgProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    }catch (Exception ex){}
+                    if (photoFile != null) {
+                        try {
+                            Uri photoURI = FileProvider.getUriForFile(AgregarPedidos.this, "com.example.programacion_lll.fileprovider", photoFile);
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(takePictureIntent, 1);
+                        }catch (Exception ex){
+                            Toast.makeText(getApplicationContext(), "Error Toma Foto: "+ ex.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                Bitmap imageBitmap = BitmapFactory.decodeFile(urlCompletaImg);
+                imgProducto.setImageBitmap(imageBitmap);
+            }
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        if (resultCode == RESULT_OK&&requestCode==UPLOAD_PICTURE) {
+
+            Uri targetURI = data.getData();
+            Bitmap bitmap=null;
+
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetURI));
+                mImageView.setImageBitmap(bitmap);
+                mCaptureOrUploadBitmap = bitmap;
+
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "imagen_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if( storageDir.exists()==false ){
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        urlCompletaImg = image.getAbsolutePath();
+        return image;
+    }
+
+    private void createProductImageFile(){
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File directory = contextWrapper.getDir("Pictures", Context.MODE_PRIVATE);
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File myPath = new File(directory, imageFileName + ".jpg");
+
+        mProductImagePath = myPath.toString();
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myPath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            mCaptureOrUploadBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
