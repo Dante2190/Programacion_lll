6dd0c449307d71817d8254a548cac981c140cca5
Dante2190 <denzel2190@gmail.com>
2020-11-12 04:16:37 +0100

    aaaaaaa

--- app/src/main/java/com/example/programacion_lll/MainActivity.java
+++ app/src/main/java/com/example/programacion_lll/MainActivity.java
@@ -12,12 +12,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 import java.util.HashMap;
 import java.util.Map;
